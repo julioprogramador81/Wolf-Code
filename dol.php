e1854b76d105364f6308b6b65de97d7db5f58886
julioprogramador81 <100220799+julioprogramador81@users.noreply.github.com>
2024-01-15 13:10:49 -0300

    dol.php

---
+++ dol.php
@@ -0,0 +1,12 @@
+<?php
+class SimpleClass
+{
+    // declaração de propriedade
+    public $var = 'um valor padrão';
+
+    // declaração de método
+    public function displayVar() {
+        echo $this->var;
+    }
+}
+?>
